a87e188272f58d7aa8932677b13e73799b7d242e
rajkumar060301 <raj.kumar_cs19@gla.ac.in>
2023-10-09 17:17:34 +0530

    add router of save for modal value and send data this router

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,5 @@ Route::get('/register', [UserControllers::class, 'index'])->name('users.index');
 Route::delete('/users/{id}', [UserControllers::class, 'delete'])->name('users.delete');
 Route::get('/update/{id}/{full_name}/{email}/{phone}/{address}', [UserControllers::class, 'showUpdateForm'])->name('update');
 Route::put('/users/{id}', [UserControllers::class, 'update'])->name('users.update');
-
 Route::get('/logout', [UserControllers::class, 'logout'])->name('logout');
+Route::post('/home', [UserControllers::class, 'add'])->name('home.dataTabel');
